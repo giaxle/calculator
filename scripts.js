cf7395af3f6fa7394514d5bb75c4975b522fab5e
Giaxle <giaxle@outlook.com>
2021-08-27 17:19:39 -0400

    added calculate function, event listeners for calculator buttons, code to display history and output values

---
+++ scripts.js
@@ -0,0 +1,88 @@
+const container = document.querySelector('.container');
+const calculator = container.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator-keys');
+
+const history = document.getElementById('history-value');
+const output = document.getElementById('output-value');
+
+// calculate function
+const calculate = (n1, operator, n2) => {
+    let result = '';
+    if (operator === 'add') {
+        result = parseFloat(n1) + parseFloat(n2);
+    } else if (operator === 'subtract') {
+        result = parseFloat(n1) - parseFloat(n2);
+    } else if (operator === 'multiply') {
+        result = parseFloat(n1) * parseFloat(n2);
+    } else if (operator === 'divide') {
+        result = parseFloat(n1) / parseFloat(n2);
+    }
+    return result;
+}
+
+// adding event listener to each button and display code 
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+    const key = e.target
+    const action = key.dataset.action;
+    
+    const keyContent = key.textContent;
+    const displayHistory = history.textContent;
+    const displayOutput = output.textContent;
+    
+    Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
+
+    const previousKeyType = calculator.dataset.previousKeyType;
+
+    if (!action) {
+        // this is for the history queue
+        if (displayHistory === '') {
+            history.textContent = keyContent;
+        } else {
+            history.textContent = displayHistory + keyContent;
+        }
+
+        // display for output and immediate number value
+        if (displayOutput === '' || previousKeyType === 'operator') {
+            calculator.dataset.previousKeyType = '';
+            output.textContent = keyContent;
+        } else {
+            output.textContent = displayOutput + keyContent;
+        }   
+        console.log(key.value)
+    }
+
+    // checks for operator
+    if (
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide'
+    ) {
+        history.textContent = displayHistory + keyContent;
+        key.classList.add('is-depressed');
+        calculator.dataset.previousKeyType = 'operator';
+
+        calculator.dataset.firstValue = displayOutput;
+        calculator.dataset.operator = action;
+        // console.log(action);
+    }
+
+    if (action === 'clear') {
+        calculator.dataset.firstValue = '';
+        calculator.dataset.operator = '';
+        calculator.dataset.secondValue = '';
+        output.textContent = '';
+        history.textContent = '';
+        console.log(action);
+    }
+
+    if (action === 'equal') {
+        const firstValue = calculator.dataset.firstValue;
+        const operator = calculator.dataset.operator;
+        const secondValue = displayOutput
+        output.textContent = calculate(firstValue, operator, secondValue);
+    }
+    }
+    
+})

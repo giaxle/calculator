ab73c940f47b9ff33afad48d2dbf4e2e9d823259
Giaxle <giaxle@outlook.com>
2021-08-30 17:51:13 -0400

    adding code for consecutive equations

--- scripts.js
+++ scripts.js
@@ -5,17 +5,27 @@ const keys = calculator.querySelector('.calculator-keys');
 const history = document.getElementById('history-value');
 const output = document.getElementById('output-value');
 
+calculator.dataset.firstValue = '';
+calculator.dataset.operator = '';
+calculator.dataset.secondValue = '';
+
+
 // calculate function
 const calculate = (n1, operator, n2) => {
     let result = '';
     if (operator === 'add') {
+        // add(result, n1, n2);
         result = parseFloat(n1) + parseFloat(n2);
     } else if (operator === 'subtract') {
         result = parseFloat(n1) - parseFloat(n2);
     } else if (operator === 'multiply') {
         result = parseFloat(n1) * parseFloat(n2);
     } else if (operator === 'divide') {
-        result = parseFloat(n1) / parseFloat(n2);
+        if (n1 === '0' || n2 === '0') {
+            result = 'It\'s not very effective';
+        } else {
+            result = parseFloat(n1) / parseFloat(n2);
+        }
     }
     return result;
 }
@@ -30,7 +40,7 @@ keys.addEventListener('click', e => {
     const displayHistory = history.textContent;
     const displayOutput = output.textContent;
     
-    Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
+    // Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
 
     const previousKeyType = calculator.dataset.previousKeyType;
 
@@ -59,13 +69,22 @@ keys.addEventListener('click', e => {
         action === 'multiply' ||
         action === 'divide'
     ) {
-        history.textContent = displayHistory + keyContent;
-        key.classList.add('is-depressed');
+        if (calculator.dataset.firstValue !== '') {
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayOutput
+            output.textContent = calculate(firstValue, operator, secondValue);
+            calculator.dataset.firstValue = output.textContent;
+            console.log('It worked!');
+        } else {
+            history.textContent = displayHistory + keyContent;
+        // key.classList.add('is-depressed');
         calculator.dataset.previousKeyType = 'operator';
-
         calculator.dataset.firstValue = displayOutput;
         calculator.dataset.operator = action;
         // console.log(action);
+        }
+
     }
 
     if (action === 'clear') {
@@ -86,3 +105,26 @@ keys.addEventListener('click', e => {
     }
     
 })
+
+const add = (result, n1, n2) => {
+    if (n1 !== '' && n2 !== '') {
+        result = n1 + n2;
+        firstValue = result;
+        return result, firstValue;
+    } else {
+        result = n1 + n2;
+        return result;
+    }
+}
+
+function checkOperand(action) {
+    if (
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide'
+    ) {
+        const firstValue = displayOutput;
+        const operator = action;
+    }
+}
